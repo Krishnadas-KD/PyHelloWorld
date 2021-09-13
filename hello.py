7748d8974e541951a51d53f41e21c457f88b62fc
KD <81668841+Krishnadas-KD@users.noreply.github.com>
2021-09-13 21:29:09 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,70 @@
+i=0
+while True:
+    if chr(i)=='h' :
+        print(chr(i),end ="")
+        i=0
+        while True:
+            if chr(i)=='e':
+                print(chr(i),end ="")
+                i=0
+                while True:
+                    if chr(i)=='l':
+                        print(chr(i),end ="")
+                        i=0
+                        while True:
+                            if chr(i)=='l' :
+                                print(chr(i),end ="")
+                                i=0
+                                while True:
+                                    if chr(i)=='o' :
+                                        print(chr(i),end ="")
+                                        i=0
+                                        while True:
+                                            if chr(i)==' ':
+                                                print(chr(i),end ="")
+                                                i=0
+                                                while True:
+                                                    if chr(i)=='w' :
+                                                        print(chr(i),end ="")
+                                                        i=0
+                                                        while True:
+                                                            if chr(i)=='o' :
+                                                                print(chr(i),end ="")
+                                                                i=0
+                                                                while True:
+                                                                    if chr(i)=='r' :
+                                                                        print(chr(i),end ="")
+                                                                        i=0
+                                                                        while True:
+                                                                            if chr(i)=='l':
+                                                                                print(chr(i),end ="")
+                                                                                i=0
+                                                                                while True:
+                                                                                    if chr(i)=='d' :
+                                                                                        print(chr(i))
+                                                                                        break
+                                                                                    i=i+1
+                                                                                break
+                                                                            i=i+1
+                                                                        break
+                                                                    i=i+1
+                                                                break
+                                                            i=i+1
+                                                        break
+                                                    i=i+1
+                                                break
+                                            i=i+1
+                                        break
+                                    i=i+1
+                                break
+                            i=i+1
+                        break
+                    i=i+1
+                break
+            i=i+1
+        break
+    i=i+1
+                                                                        
+                                                                                     
+
+
